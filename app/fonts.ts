0c21c059a38c9592502c90dc2cedaadadda9e503
Felix Jung <felixjung@gmail.com>
2023-10-16 21:13:06 -0500

    Updates chatgpt requests to use chat completion, prompt refinements.

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,10 +3,12 @@ import { Raleway, Spectral } from 'next/font/google';
 export const raleway = Raleway({
   subsets: ['latin'],
   display: 'swap',
+  preload: true,
 });
 
 export const initialLetter = Spectral({
   subsets: ['latin'],
   weight: '700',
   variable: '--font-initial-letter',
+  preload: true,
 });
